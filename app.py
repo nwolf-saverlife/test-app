830eee35381543eae7f093308dab79f24da8845d
nwolf <nwolf@saverlife.org>
2022-02-09 14:51:41 -0700

    Fix

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
