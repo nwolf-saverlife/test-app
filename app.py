3e4f8dff7530e267b85b9c4cce61bd9a02873b8a
Nelson Wolf <99222235+nwolf-saverlife@users.noreply.github.com>
2022-02-11 11:48:00 -0700

    print env vars

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! PR1</p>"
+    all_env_vars = ""
+    for var in os.environ:
+        all_env_vars += f"\n{var}={os.environ[var]}<br>\n"
+    return "<p>Hello, World!</p>\n" + all_env_vars
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
